c1d8958b6f2a56c7a35397c0a6ef5d86c8703766
Alex Shnayder <alex@shnayder.pro>
2015-11-08 06:54:18 +0600

    Fix the methods returning the original object instead of the patched one

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 'use strict';
 
 module.exports = function(yargs) {
+  // A simple way to get the underlying yargs object
+  yargs = yargs.exitProcess();
+
   var yargsChild = Object.create(yargs);
 
+  // Patch the methods so that they return our object
+  // instead of the original one
+  Object.keys(yargs).forEach(function(key) {
+    if (typeof yargs[key] !== 'function') {
+      return;
+    }
+
+    yargsChild[key] = function() {
+      var result = yargs[key].apply(this, arguments);
+      return (result === yargs) ? yargsChild : result;
+    };
+  });
+
   Object.defineProperty(yargsChild, 'argv', {
-    get: function () {
+    get: function() {
       var commandHandlers = this.getCommandHandlers();
 
       if (!commandHandlers['*']) {
@@ -16,9 +32,9 @@ module.exports = function(yargs) {
 
       Object.keys(commandHandlers).forEach(function(command) {
         var oldCommandHandler = commandHandlers[command];
-        commandHandlers[command] = function(newYargs) {
+        commandHandlers[command] = function() {
           commandTriggered = true;
-          return oldCommandHandler.call(this, newYargs);
+          return oldCommandHandler.call(this, yargsChild.reset());
         };
       });
 
